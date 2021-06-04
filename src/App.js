2916b6896606ec66381ec3d7126ba516d4eb52ea
Yuvraj Kaushal <ykaushal364@gmail.com>
2021-06-05 04:13:03 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
+import { Multiselect } from 'multiselect-react-dropdown'
 import './App.css';
 
 function App() {
+
+  const data = [
+    {Names: 'Yuvraj', id: 1},
+    {Names: 'Rajjo', id: 2},
+    {Names: 'Anmol', id: 3},
+    {Names: 'Amisha', id: 4},
+    {Names: 'Nishant', id: 5},
+    {Names: 'Ajit', id: 6},
+    {Names: 'Sachin', id: 7},
+    {Names: 'Amaan', id: 8},
+    {Names: 'Faisal', id: 9},
+    {Names: 'Mahabir', id: 10}
+  ]
+
+  const [options] = useState(data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="style">
+      <h1>Multiselect Dropdown</h1>
+      <Multiselect options={options} displayValue = "Names"/>
+    </div>   
     </div>
   );
 }
